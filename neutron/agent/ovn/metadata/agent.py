e74cc4a35dc1c2cd15e4d05cdda9a35fbfb1bab0
Ivan Zhang <sail4dream@gmail.com>
2022-06-30 21:57:41 +0800

    add qos on router gateway port

--- neutron/agent/ovn/metadata/agent.py
+++ neutron/agent/ovn/metadata/agent.py
@@ -286,6 +286,7 @@ class MetadataAgent(object):
             max=config.get_ovn_ovsdb_retry_max_interval()),
         reraise=True)
     def register_metadata_agent(self):
+        LOG.info("register metadata agent: %s %s", self.chassis, self.chassis_id)
         # NOTE(lucasagomes): db_add() will not overwrite the UUID if
         # it's already set.
         table = ('Chassis_Private' if self.has_chassis_private else 'Chassis')
